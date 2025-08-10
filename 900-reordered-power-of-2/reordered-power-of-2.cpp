68cd58021c6285894353a1f185e4c6da42b54ceb
Sarthak Gagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2025-08-10 14:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (91.45%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    unordered_set<string>st;
 
-    void buildSet(){
-        for(int p=0; p<=29; p++){
-            string s=to_string(1<<p);
-            sort(begin(s),end(s));
-            st.insert(s);
+    vector<int>getVecCount(int n){
+        vector<int>num(10,0);
+
+        while(n>0){
+            num[n%10]++;
+            n/=10;
         }
+        return num;
     }
 
+
     bool reorderedPowerOf2(int n) {
-        if(st.empty()){
-            buildSet();
+        vector<int>input=getVecCount(n);
+
+        for(int p=0; p<=29; p++){
+            if(input==getVecCount(1<<p)){
+                return true;
+            }
         }
-
-        string s=to_string(n);
-        sort(s.begin(),s.end());
-
-        if(st.find(s) != st.end()){
-            return true;
-        }
-
         return false;
-
     }
 };
