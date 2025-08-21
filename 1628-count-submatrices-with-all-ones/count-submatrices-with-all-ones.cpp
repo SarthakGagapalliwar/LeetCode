8e23edaac56b1c38eab5ebabd8cb91243a4f87cd
Sarthak Gagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2025-08-21 23:16:58 +0530

    Time: 41 ms (19.75%) | Memory: 19.2 MB (63.20%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
-    int onedArrayCount(vector<int>& vec) {
-        int cons = 0;
-        int subcount = 0;
+    int OneArr(vector<int>&arr){
+        int cons=0;
+        int res=0;
 
-        for (int &val : vec) {
-            if (val == 0) {
-                cons = 0;
-            } else {
+        for(auto& a : arr){
+            if(a==0){
+                cons=0;
+            }else{
                 cons++;
             }
-            subcount += cons;
+            res+=cons;
         }
-
-        return subcount;
+        return res;
     }
 
+
     int numSubmat(vector<vector<int>>& mat) {
+        
+
         int m = mat.size();
-        int n = mat[0].size();
+        int n=mat[0].size();
+        int res=0;
 
-        int result = 0;
 
-        for (int startRow = 0; startRow < m; startRow++) {
-            vector<int> vec(n, 1);
+        for(int startRow=0; startRow<m; startRow++){
+                vector<int>vec(n,1);
+            for(int endRow=startRow; endRow<m; endRow++){
 
-            for (int endRow = startRow; endRow < m; endRow++) {
-                for (int col = 0; col < n; col++) {
-                    vec[col] = vec[col] & mat[endRow][col];
-                }
-                result += onedArrayCount(vec);
+                    for(int col=0; col<n; col++){
+                        vec[col]=vec[col]&mat[endRow][col];
+                    }
+
+                res+=OneArr(vec);
             }
         }
 
-        return result;
+        return res;
+
     }
 };
