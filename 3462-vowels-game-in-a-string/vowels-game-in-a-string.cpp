aff6f82658af86c62e4e548684bc6a8e44e0c3a6
Sarthak Gagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2025-09-12 12:31:11 +0530

    Time: 1 ms (91.79%) | Memory: 19.1 MB (53.73%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -4,18 +4,10 @@ public:
         int cnt=0;
         for(int i=0; i<s.size(); i++){
             if(s[i]=='a' || s[i]=='e' || s[i]=='o' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
-                cnt++;
-            }
-        }
-
-        while(cnt){
-            if(cnt%2 != 0){
                 return true;
-            }
-            else{
-                cnt--;
             }
         }
         return false;
+
     }
 };
