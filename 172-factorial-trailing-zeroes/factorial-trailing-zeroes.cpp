78b7b008463e1c4cd27a474bceb0a1308ebeb2f3
Sarthak Gagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2025-09-24 15:45:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (53.03%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int result=0;
+        int num=n/5;
+        while(num>0){
+            result+=num;
+            num=num/5;
+        }
+        return result;
+
+    }
+};
