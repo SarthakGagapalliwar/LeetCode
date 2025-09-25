ade91dfe6a170dac322116db91f2e2f3a1007516
Sarthak Gagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2025-09-25 18:49:54 +0530

    Time: 4 ms (7.22%) | Memory: 13.3 MB (55.55%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,23 @@
+//  bottom to up
+
+class Solution {
+public:
+    long long minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        // Base case: last row = same as triangle
+        for (int j = 0; j < n; j++) {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        // Bottom-up DP
+        for (int i = n-2; i >=0; i--) {
+            for (int j = 0; j <=i; j++) {
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        
+        return dp[0][0];
+    }
+};
