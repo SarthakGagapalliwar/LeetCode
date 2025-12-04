df7da866a81f1290456537d849b795d37dc1aba0
Sarthak Gagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2025-12-04 22:00:42 +0530

    Time: 5 ms (87.83%) | Memory: 16.8 MB (100.00%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-    int countCollisions(string D) {
+    static int countCollisions(string& D) {
         int n=D.size();
         if (n==1) return 0;
-        int l=0, r=n-1;
-        while (D[l]=='L') l++;
-        while (l<r && D[r]=='R') r--;
+        int l=D.find_first_not_of('L');
+        if (l==-1) l=n-1;
+        int r=D.find_last_not_of('R');
+        if (r==-1) r=0;
         if (l>=r) return 0;
-        int col=0;
-    
-        for( ; l<=r; l++){
-            while(D[l]=='R'){
-                l++;
-                col++;
-            }
-            if (D[l]=='L') col++;
-        }
-        return col;      
+        return count_if(D.begin()+l, D.begin()+(r+1), 
+        [](char c){ return c!='S';});      
     }
 };
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
