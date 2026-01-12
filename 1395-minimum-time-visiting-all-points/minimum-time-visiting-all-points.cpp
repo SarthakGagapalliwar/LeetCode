1c355603b9e3a52c78e8e84d6bd9af5f74e78b6e
Sarthak Gagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2026-01-12 22:02:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (61.26%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    int maxNorm(vector<int>& P, vector<int>& Q){
-        return max(abs(P[0]-Q[0]), abs(P[1]-Q[1]));
-    }
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int ans=0;
-        vector<int> prev=points[0];
-        for (auto& P: points){
-            ans+=maxNorm(P, prev);
-            prev=P;
+        int ans = 0;
+        for(int i = 1; i < points.size(); i++) {
+            ans += max(abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1]));
         }
         return ans;
     }
